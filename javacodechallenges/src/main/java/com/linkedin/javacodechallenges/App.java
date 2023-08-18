6e27cd2483db76e68afbff0c5a4c202c0a432db8
thausyn <142385415+thausyn@users.noreply.github.com>
2023-08-18 08:46:09 +0000

    even or odd lesson

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,24 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static boolean isEven(int userNum){
+        return userNum % 2 == 0;
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println("Is the number even? " + isEven(userNum));
+        scanner.close();
     }
 }
